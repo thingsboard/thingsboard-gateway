4714a520b05f0c710ed1e252490fcca76f3dc0ca
Andrew Shvayka <ashvayka@thingsboard.io>
2017-01-29 03:12:27 +0200

    SSL implementation

--- src/main/java/org/thingsboard/gateway/service/MqttGatewaySecurityConfiguration.java
+++ src/main/java/org/thingsboard/gateway/service/MqttGatewaySecurityConfiguration.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © ${project.inceptionYear}-2017 The Thingsboard Authors
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,9 +21,9 @@ import org.eclipse.paho.client.mqttv3.internal.security.SSLSocketFactoryFactory;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.StringUtils;
 
+import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.util.Properties;
 
 /**
@@ -35,6 +35,7 @@ public class MqttGatewaySecurityConfiguration {
     private String accessToken;
     private String keystore;
     private String keystorePassword;
+    private String keystoreKeyAlias;
     private String truststore;
     private String truststorePassword;
 
@@ -75,8 +76,27 @@ public class MqttGatewaySecurityConfiguration {
         if (this.isTokenBased()) {
             return sha256(this.getAccessToken().getBytes(StandardCharsets.UTF_8));
         } else {
-            //TODO: hash from public key;
-            throw new RuntimeException("Not supported!");
+            try {
+                FileInputStream is = new FileInputStream(this.getKeystore());
+
+                KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+                keystore.load(is, this.getKeystorePassword().toCharArray());
+
+                Key key = keystore.getKey(this.getKeystoreKeyAlias(), this.getKeystorePassword().toCharArray());
+                if (key instanceof PrivateKey) {
+                    // Get certificate of public key
+                    java.security.cert.Certificate cert = keystore.getCertificate(this.getKeystoreKeyAlias());
+
+                    // Get public key
+                    PublicKey publicKey = cert.getPublicKey();
+
+                    return sha256(publicKey.getEncoded());
+                } else {
+                    throw new RuntimeException("No public key!");
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
