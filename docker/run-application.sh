fc9f97fb9ae1c414b9836b63c452dd9f0f481d4d
Yannik Korzikowski <yannik@korzikowski.de>
2019-09-02 21:49:05 +0200

    improved the usage of docker     - dpkg installation is executed on image build now     - the jar is executed in front so the logging is available via docker log     - no warning messages about missing openjdk

--- docker/run-application.sh
+++ docker/run-application.sh
@@ -15,16 +15,11 @@
 # limitations under the License.
 #
 
-dpkg -i /tb-gateway.deb
-
 # Copying env variables into conf files
 printenv | awk -F "=" '{print "export " $1 "='\''" $2 "'\''"}' >> /usr/share/tb-gateway/conf/tb-gateway.conf
 
 cat /usr/share/tb-gateway/conf/tb-gateway.conf
 
 echo "Starting 'TB-gateway' service..."
-service tb-gateway start
 
-# Wait until log file is created
-sleep 5
-tail -f /var/log/tb-gateway/tb-gateway.log
+java -jar /opt/biot/tb_test/tb-gateway/bin/tb-gateway.jar
