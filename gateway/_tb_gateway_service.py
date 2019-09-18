a91a345dd4b1a502c03595327e3ca2bc1bc046e8
zbeacon <ibarkov@thingsboard.io>
2019-09-18 15:22:05 +0300

    Added sending information from storage to ThingsBoard

--- gateway/_tb_gateway_service.py
+++ gateway/_tb_gateway_service.py
@@ -1,19 +1,12 @@
 import logging
 import time
 import yaml
-from abc import ABC,abstractmethod
 from json import load, loads, dumps
-from connectors.mqtt.mqtt_connector import MqttConnector
-from queue import Queue
-from threading import Lock
-
-from apscheduler.events import EVENT_JOB_ERROR
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-from apscheduler.schedulers.background import BackgroundScheduler
 
 from gateway.tb_client import TBClient
-from tb_client.tb_gateway_mqtt import TBGatewayMqttClient
 from tb_utility.tb_utility import TBUtility
+from threading import Thread
+from connectors.mqtt.mqtt_connector import MqttConnector
 from storage.memory_event_storage import MemoryEventStorage
 
 
@@ -25,197 +18,23 @@ class TBGatewayService:
         with open(config_file) as config:
             config = yaml.safe_load(config)
             self.available_connectors = {}
+            self.__connected_devices = {}
             self.__connector_incoming_messages = {}
-
-            # dict_extensions_settings = config["extensions"]
+            self.__Send_Thread = Thread(target=self.__send_data_from_storage, daemon=True)
             if config["storage"]["type"] == "memory":
                 self.__event_storage = MemoryEventStorage(config["storage"])
             else:
-                pass  # TODO Add file storage
-            # dict_performance_settings = config.get("performance")
-            # data_folder_path = dict_storage_settings["path"]
-            # max_records_per_file = dict_storage_settings["max_records_per_file"]
-            # max_records_between_fsync = dict_storage_settings["max_records_between_fsync"]
-            # max_file_count = dict_storage_settings["max_file_count"]
-            # read_interval = dict_storage_settings["read_interval"] / 1000
-            # max_read_record_count = dict_storage_settings["max_read_record_count"]
-            # if dict_performance_settings:
-            #     number_of_processes = TBUtility.get_parameter(dict_performance_settings, "processes_to_use", 20)
-            #     number_of_workers = TBUtility.get_parameter(dict_performance_settings, "additional_threads_to_use", 5)
-
-            # self.dict_ext_by_device_name = {}
-            # self.dict_rpc_handlers_by_device = {}
-            # self.lock = Lock()
-
-            # # initialize scheduler
-            # executors = {'default': ThreadPoolExecutor(number_of_workers)}
-            # if number_of_processes > 1:
-            #     executors.update({'processpool': ProcessPoolExecutor(number_of_processes)})
-            # self.scheduler = BackgroundScheduler(executors=executors)
-            # # TODO: Investigate
-            # # self.scheduler.add_listener(TBGateway.listener, EVENT_JOB_ERROR)
-            # self.scheduler.start()
-
-            # initialize client
+                pass
             self.tb_client = TBClient(config["thingsboard-client"])
             self.tb_client.connect()
 
             self.__load_connectors(config)
             self.__connect_with_connectors()
+            self.__TEST_PUT_FOR__Send_Thread.start()
+            self.__Send_Thread.start()
 
             while True:
                 time.sleep(.1)
-
-    #         def rpc_request_handler(self, request_body):
-    #             method = request_body["data"]["method"]
-    #             device = request_body.get("device")
-    #             # device = "MJ_HT_V1_4c65a8df8e3f"
-    #             #todo this is for testing, in worst case test for modbus
-    #             extension_class = self.gateway.dict_ext_by_device_name[device]
-    #             values = None
-    #             # todo remove if true, uncomment if type..., remove connect(test device), they were for testing purpses
-    #             try:
-    #                 dict_device_handlers = self.gateway.dict_rpc_handlers_by_device[device]
-    #             except KeyError:
-    #                 pass
-    #                 self.send_rpc_reply("no device {} found".format(device))
-    #
-    #                 log.error("no device {} found".format(device))
-    #                 return
-    #
-    #             # todo rework handler call to give handler device name etc
-    #
-    #             resp = None
-    #             # extension_class.handler
-    #
-    #             # todo remove after testing (30.6)
-    #             if True:
-    #             # if type(extension_class) == TB_MQTT_Extension:
-    #                 resp = self.gateway.dict_ext_by_device_name[device].handler(request_body)
-    #
-    #             # todo rework if type(...) == xx -> ext.handler(params)
-    #             # todo need we add another thread for rpc responses? maybe no
-    #             elif type(extension_class) == TBModbus:
-    #                 try:
-    #                     handler = dict_device_handlers[method]
-    #                 except KeyError:
-    #                     self.send_rpc_reply(device, request_body["data"]["id"], {"error": "Unsupported RPC method"})
-    #                     log.error('"error": "Unsupported RPC method": {}'.format(request_body))
-    #                     return
-    #
-    #                 if type(handler) == str:
-    #                     m = import_module("extensions.modbus."+handler).Extension()
-    #
-    #                     params = None
-    #                     try:
-    #                         params = request_body["data"]["params"]
-    #                         values = m.rpc_handler(method, params)
-    #                     except KeyError:
-    #                         pass
-    #                 elif type(handler) == dict:
-    #                     values = handler
-    #                     values.update({"deviceName": device})
-    #                 else:
-    #                     log.error("rpc handler not in dict format nor in string path to python file")
-    #                     return
-    #                 if "tag" not in values:
-    #                     self.send_rpc_reply(device, request_body["data"]["id"], {"ErrorWriteTag": "No tag found"})
-    #                     log.error('"ErrorWriteTag": "No tag found": {}'.format(request_body))
-    #                     return
-    #                 resp = self.gateway.dict_ext_by_device_name[device](values)
-    #                 if resp:
-    #                     self.gw_send_rpc_reply(device, request_body["data"]["id"], resp)
-    #
-    #             elif type(extension_class) == TBBluetoothLE:
-    #                 try:
-    #                     handler = dict_device_handlers[method]
-    #                 except KeyError:
-    #                     self.send_rpc_reply(device, request_body["data"]["id"], {"error": "Unsupported RPC method"})
-    #                     log.error('"error": "Unsupported RPC method": {}'.format(request_body))
-    #                     return
-    #                 self.gateway.dict_ext_by_device_name[device].rpc_handler(request_body, handler)
-    #             if resp:
-    #                 self.gw_send_rpc_reply(device, request_body["data"]["id"], resp)
-    #
-    #         self.mqtt_gateway.devices_server_side_rpc_request_handler = rpc_request_handler
-    #         # connect devices from file
-    #         self.mqtt_gateway.connect_devices_from_file(self.mqtt_gateway)
-    #         # initialize connected device logging
-    #
-    #         device_thread_interval = TBUtility.get_parameter(config, "device_storage_thread_read_interval", 1000) / 1000
-    #         self.q = Queue()
-    #
-    #         TBDeviceStorage(self, device_thread_interval)
-    #
-    #
-    #         # initialize event_storage
-    #         self.event_storage = TBEventStorage(data_folder_path, max_records_per_file, max_records_between_fsync,
-    #                                             max_file_count, read_interval, max_read_record_count, self.scheduler,
-    #                                             self)
-    #
-    #         # initialize extensions
-    #         for ext_id in dict_extensions_settings:
-    #             extension = dict_extensions_settings[ext_id]
-    #             extension_type = extension["extension_type"]
-    #             extension_is_enabled = extension["enabled"]
-    #             if extension_type == "Modbus" and extension_is_enabled:
-    #                 conf = TBUtility.get_parameter(extension, "config_filename", "ModbusConfig.json")
-    #                 with open(conf, "r") as config_file:
-    #                     for modbus_config in load(config_file)["servers"]:
-    #                         TBModbus(modbus_config, self.scheduler, self, ext_id)
-    #             elif extension_type == "BLE" and extension_is_enabled:
-    #                 conf = TBUtility.get_parameter(extension, "config_filename", "BLEConfig.json")
-    #                 TBBluetoothLE(self, conf, ext_id)
-    #             elif extension_type == "MQTT" and extension_is_enabled:
-    #                 conf = TBUtility.get_parameter(extension, "config_filename", "MQTTConfig.json")
-    #                 with open(conf, "r") as config_file:
-    #                     for broker_config in load(config_file)["brokers"]:
-    #                         TB_MQTT_Extension(broker_config, self, ext_id)
-    #             elif extension_type == "OPC-UA" and extension_is_enabled:
-    #                 log.warning("OPC UA isn't implemented yet")
-    #             elif extension_is_enabled:
-    #                 log.error("unknown extension_type: {}".format(extension["extension_type"]))
-    #             else:
-    #                 log.debug("id {}, type {} is not enabled, skipping...".format(ext_id, extension_type))
-    #
-    #
-    # def on_device_connected(self, device_name, handler, rpc_handlers):
-    #     self.q.put((True, device_name, handler, rpc_handlers))
-    #
-    # def on_device_disconnected(self, device_name):
-    #     self.q.put((False, device_name))
-    #
-    # # todo implement methods
-    # def send_attributes_to_storage(self, data, device):
-    #     self.event_storage.write(dumps({"eventType": "ATTRIBUTES", "device": device, "data": data}) + "\n")
-    #
-    # def send_telemetry_to_storage(self, data, device):
-    #     self.event_storage.write(dumps({"eventType": "TELEMETRY", "device": device, "data": data}) + "\n")
-    #
-    # def send_data_to_storage(self, data, type_of_data, device):
-    #     if type_of_data == "telemetry":
-    #
-    #         self.event_storage.write(dumps({"eventType": "TELEMETRY", "device": device, "data": data}) + "\n")
-    #     else:
-    #         self.event_storage.write(dumps({"eventType": "ATTRIBUTES", "device": device, "data": data}) + "\n")
-    #
-    # def send_data_to_tb(self, data):
-    #     for event in data:
-    #         device = event["device"]
-    #         if device not in self.dict_ext_by_device_name:
-    #             self.mqtt_gateway.gw_connect_device("device").wait_for_publish()
-    #
-    #             # todo add wait for publish (get)??
-    #         if event["eventType"] == "TELEMETRY":
-    #             self.mqtt_gateway.gw_send_telemetry(device, event["data"])
-    #         else:
-    #             self.mqtt_gateway.gw_send_attributes(device, event["data"]["values"])
-    #         # todo add checking if messages arrived
-    #     return True
-    #
-    # @staticmethod
-    # def listener(event):
-    #     log.exception(event.exception)
 
     def __load_connectors(self, config):
         self._connectors_configs = {}
@@ -256,3 +75,21 @@ class TBGatewayService:
             log.debug('Connector "%s" - Saved information - %s', connector_name, json_data)
         else:
             log.error('Data from connector "%s" cannot be saved.')
+
+    def __send_data_from_storage(self):
+        while True:
+            events = self.__event_storage.get_event_pack()
+            if events:
+                for event in events:
+                    current_event = loads(event)
+                    if current_event["deviceName"] not in self.__connected_devices:
+                        self.tb_client._client.gw_connect_device(current_event["deviceName"]).wait_for_publish()
+                    self.__connected_devices[current_event["deviceName"]] = current_event["deviceName"]
+                    if current_event.get("telemetry"):
+                        data_to_send = loads('{"ts": %i,"values": %s}'%(time.time(), ','.join(dumps(param) for param in current_event["telemetry"])))
+                        self.tb_client._client.gw_send_telemetry(current_event["deviceName"], data_to_send)
+                    if current_event.get("attributes"):
+                        data_to_send = loads('%s'%( ','.join(dumps(param) for param in current_event["attributes"])))
+                        self.tb_client._client.gw_send_attributes(current_event["deviceName"], data_to_send)
+            self.__event_storage.event_pack_processing_done()
+            time.sleep(5)
