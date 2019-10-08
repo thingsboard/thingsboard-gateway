1b9cc3a1f3df7b9505ca43879331de9bafc7a83d
zbeacon <ibarkov@thingsboard.io>
2019-10-08 12:59:18 +0300

    Added comments to explain the mqtt custom converter code.

--- extensions/mqtt/custom_mqtt_uplink_converter.py
+++ extensions/mqtt/custom_mqtt_uplink_converter.py
@@ -13,20 +13,22 @@ class CustomMqttUplinkConverter(MqttUplinkConverter):
 
     def convert(self, topic, body):
         try:
+            '''  getting device name from topic, next line will get all data after last '/' symbol 
+             in this case: if topic = 'devices/themperature/sensor1' device name will be 'sensor1'.'''
             self.dict_result["deviceName"] = topic.split("/")[-1]
             self.dict_result["deviceType"] = "Thermostat"  # just hardcode this
-            self.dict_result["telemetry"] = []
-            bytes_to_read = body.replace("0x", "")
-            converted_bytes = bytearray.fromhex(bytes_to_read)
+            self.dict_result["telemetry"] = []  # template for telemetry array
+            bytes_to_read = body.replace("0x", "")  # Replacing the 0x (if '0x' in body), needs for converting to bytrearray
+            converted_bytes = bytearray.fromhex(bytes_to_read)  # Converting incoming data to bytearray
             if self.__config.get("extension-config") is not None:
-                for telemetry_key in self.__config["extension-config"]:
+                for telemetry_key in self.__config["extension-config"]:  # Processing every telemetry key in config for extension
                     value = 0
-                    for current_byte_position in range(self.__config["extension-config"][telemetry_key]):
-                        value = value*256 + converted_bytes.pop(0)
-                    telemetry_to_send = {telemetry_key.replace("Bytes", ""): value}
-                    self.dict_result["telemetry"].append(telemetry_to_send)
+                    for current_byte_position in range(self.__config["extension-config"][telemetry_key]):  # reading every value with value length from config
+                        value = value*256 + converted_bytes.pop(0)  # process and remove byte from processing
+                    telemetry_to_send = {telemetry_key.replace("Bytes", ""): value}  # creating telemetry data for sending into Thingsboard
+                    self.dict_result["telemetry"].append(telemetry_to_send)  # adding data to telemetry array
             else:
-                self.dict_result["telemetry"] = {"data": int(body, 0)}
+                self.dict_result["telemetry"] = {"data": int(body, 0)}  # if no specific configuration in config file - just send data which received
             return self.dict_result
 
         except Exception as e:
