a93208347d61142af4167449f99d3ed212e5c61b
zbeacon <ibarkov.ua@gmail.com>
2019-10-30 14:09:38 +0200

    Updated copyright

--- make_packages.sh
+++ make_packages.sh
@@ -1,3 +1,17 @@
+#     Copyright 2019. ThingsBoard
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
 if [ "$1" = "clean" ] || [ "$1" = "only_clean" ] ; then
   sudo rm -rf /var/log/thingsboard-gateway/
   sudo rm -rf deb_dist/
@@ -9,8 +23,6 @@ if [ "$1" = "clean" ] || [ "$1" = "only_clean" ] ; then
   sudo rm -rf build/
   sudo apt remove python3-thingsboard-gateway -y
 fi
-
-#IFS=':' read -ra env_path <<< "$PATH"
 
 CURRENT_USER=$USER
 
