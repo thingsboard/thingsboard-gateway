d560fbf0f639cc89388b16ed179ad8b61fe76356
Shiv Shankar Maurya <12098397+ssmaurya@users.noreply.github.com>
2019-11-09 14:06:12 +0530

    Fix for issue #70
    Device type field in the GUI is not working #70.

--- src/main/java/org/thingsboard/gateway/service/MqttMessageSender.java
+++ src/main/java/org/thingsboard/gateway/service/MqttMessageSender.java
@@ -83,6 +83,13 @@ public class MqttMessageSender implements Runnable {
                         log.debug("Sending message [{}]", message);
                         Future<Void> publishFuture = publishMqttMessage(message);
                         outgoingQueue.add(publishFuture);
+                        // If there is Connect,Attributes and Telemetry message sent for a device back to back
+                        // device type created is Default in platform instead of actual device type.
+                        // This is due to device creation based on attributes/telemetry messaage before connect message in platform
+                        // Delaying message delivery solves the issue. This could impact total message delivery
+                        // from gateway to platform
+                        Thread.sleep(10);
+
                     }
                 } else {
                     Thread.sleep(persistence.getPollingInterval());
