8f3fab47c4395ff2af21b22b35206848ad2e3722
m-krasikov <growield@gmail.com>
2019-12-19 19:19:24 +0200

    Improvements to the docker build process

--- docker/start-gateway.sh
+++ docker/start-gateway.sh
@@ -1,12 +1,12 @@
 #
-#CONF_FOLDER="/etc/thingsboard-gateway/config"
-#firstlaunch=${CONG_FOLDER}/.firstlaunch
+CONF_FOLDER="/etc/thingsboard-gateway/config"
+firstlaunch=${CONF_FOLDER}/.firstlaunch
 
-#if [ ! -f ${firstlaunch} ]; then
-#    cp -r /default-config/config/* /etc/thingsboard-gateway/config/
-#    cp -r /default-config/extentions/* /var/lib/thingsboard_gateway/extensions/
-#    touch ${firstlaunch}
-#fi
-
+if [ ! -f ${firstlaunch} ]; then
+    cp -r /default-config/config/* /etc/thingsboard-gateway/config/
+    cp -r /default-config/extensions/* /var/lib/thingsboard_gateway/extensions/
+    touch ${firstlaunch}
+    echo "#Remove this file only if you want to recreate default config files! This will overwrite exesting files" > ${firstlaunch}
+fi
 
 /usr/bin/python3 -c "from thingsboard_gateway.tb_gateway import daemon; daemon()"
