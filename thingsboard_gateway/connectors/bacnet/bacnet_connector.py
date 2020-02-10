76b84ba10c1234bbd5a7306cfee6eb661961a730
zbeacon <ibarkov.ua@gmail.com>
2020-02-10 15:11:15 +0200

    Refactoring

--- thingsboard_gateway/connectors/bacnet/bacnet_connector.py
+++ thingsboard_gateway/connectors/bacnet/bacnet_connector.py
@@ -12,3 +12,12 @@
 #      See the License for the specific language governing permissions and
 #      limitations under the License.
 
+
+from thingsboard_gateway.connectors.connector import Connector, log
+
+
+class BacknetConnector(Connector):
+    def __init__(self):
+        pass
+
+
