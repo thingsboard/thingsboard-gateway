4ba232d06bf8b235329cf5bdb5dda778f05dae98
zbeacon <ibarkov.ua@gmail.com>
2020-02-18 13:08:41 +0200

    Basic implementation for REST connector

--- thingsboard_gateway/connectors/rest/rest_connector.py
+++ thingsboard_gateway/connectors/rest/rest_connector.py
@@ -4,6 +4,47 @@ from threading import Thread
 from thingsboard_gateway.tb_utility.tb_utility import TBUtility
 
 
+'''
+
+url: http://127.0.0.1/test_device
+
+method: POST
+
+HEADER: 
+
+Authorization: Basic dXNlcjpwYXNzd2Q=
+
+BODY:
+
+body = {
+            "name": "number 2",
+            "sensorModel": "0AF0CE",
+            "temp": 25.8,
+        }
+
+mapping_dict = {
+                "/test_device":
+                    {
+                        "converter":JsonRestUplinkConverter(config["converter"]),
+                        "rpc":"...",
+                        "attributeUpdates":"..."
+                    }
+                }
+
+mapping_dict["/test_device"]["converter"].convert("/test_device", body)
+
+#TODO: 
+1 Create endpoints (with/without authorization and methods from the config)
+1.1 Initialize converters for endpoints
+1.2 Create a dictionary for data processing 
+2 Run application on open() function on host and port from the config.
+3 On receiving message: convert data and send_to_storage 
+
+
+'''
+
+
+
 class HttpConnector(Connector, Thread):
     def __init__(self, gateway, config, connector_type):
         super().__init__()
