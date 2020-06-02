2161ebfb3bb5613ae7cd1db65a5aac288ed99cbc
zbeacon <ibarkov.ua@gmail.com>
2020-06-02 08:17:18 +0300

    Library installation improvements

--- thingsboard_gateway/tb_utility/tb_utility.py
+++ thingsboard_gateway/tb_utility/tb_utility.py
@@ -165,5 +165,6 @@ class TBUtility:
             except Exception:
                 pass
             if current_package_version is None or current_package_version != version:
-                result = check_call([executable, "-m", "pip", "install", package + "==" + version, "--user"])
+                installation_sign = "==" if ">=" not in version else ""
+                result = check_call([executable, "-m", "pip", "install", package + installation_sign + version, "--user"])
         return result
