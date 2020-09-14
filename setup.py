05455f27e2522fa53e9904d0e6c5a745d589d83d
zbeacon <ibarkov.ua@gmail.com>
2020-09-14 13:43:32 +0300

    For release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "2.5.0"
+VERSION = "2.5.1"
 
 setup(
     version=VERSION,
--- thingsboard-gateway.spec
+++ thingsboard-gateway.spec
@@ -1,6 +1,6 @@
 %define name thingsboard-gateway
-%define version 2.5.0
-%define unmangled_version 2.5.0
+%define version 2.5.1
+%define unmangled_version 2.5.1
 %define release 1
 
 Summary: Thingsboard Gateway for IoT devices.
