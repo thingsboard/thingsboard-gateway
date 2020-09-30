01e608c8850acdefff58ed1e7ca5658f2155aea7
agtokty <agitoktay@gmail.com>
2020-09-30 08:02:00 +0300

    fix copy command

--- generate_deb_package.sh
+++ generate_deb_package.sh
@@ -30,8 +30,8 @@ if [ "$1" != "only_clean" ] ; then
   echo "Creating sources for DEB package..."
   python3 setup.py --command-packages=stdeb.command bdist_deb
   echo "Adding the files, scripts and permissions in the package"
-  sudo cp -r thingsboard_gateway/extensions for_build/etc/thingsboard-gateway/extensions
-  sudo cp -r thingsboard_gateway/config for_build/etc/thingsboard-gateway/config
+  sudo cp -r thingsboard_gateway/extensions for_build/etc/thingsboard-gateway/
+  sudo cp -r thingsboard_gateway/config for_build/etc/thingsboard-gateway/
   sudo cp -r for_build/etc deb_dist/thingsboard-gateway-$CURRENT_VERSION/debian/python3-thingsboard-gateway
   sudo cp -r for_build/var deb_dist/thingsboard-gateway-$CURRENT_VERSION/debian/python3-thingsboard-gateway
   sudo cp -r -a for_build/DEBIAN deb_dist/thingsboard-gateway-$CURRENT_VERSION/debian/python3-thingsboard-gateway
