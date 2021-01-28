f8ccdf81613c4793627ccdcadf1ab03703117e97
zbeacon <ibarkov.ua@gmail.com>
2021-01-28 12:18:57 +0200

    Refactored bytes uplink converter in Modbus to correcty handle bytes payload

--- thingsboard_gateway/connectors/modbus/bytes_modbus_uplink_converter.py
+++ thingsboard_gateway/connectors/modbus/bytes_modbus_uplink_converter.py
@@ -119,6 +119,9 @@ class BytesModbusUplinkConverter(ModbusConverter):
         if lower_type == "string":
             decoded = decoder_functions[type_](objects_count * 2)
 
+        elif lower_type == "bytes":
+            decoded = decoder_functions[type_](size=objects_count*2)
+
         elif decoder_functions.get(lower_type) is not None:
             decoded = decoder_functions[lower_type]()
 
@@ -145,7 +148,7 @@ class BytesModbusUplinkConverter(ModbusConverter):
         elif isinstance(decoded, bytes) and lower_type == "string":
             result_data = decoded.decode('UTF-8')
         elif isinstance(decoded, bytes) and lower_type == "bytes":
-            result_data = decoded
+            result_data = decoded.hex()
         elif isinstance(decoded, list):
             if configuration.get('bit') is not None:
                 result_data = decoded[configuration['bit']]
