49910b42c3eb2e53eeefcd49880d6661bcd7d640
zbeacon <ibarkov.ua@gmail.com>
2021-03-22 12:55:11 +0200

    For release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "2.5.5"
+VERSION = "2.6"
 
 setup(
     version=VERSION,
