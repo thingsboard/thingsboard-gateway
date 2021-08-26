44642e4ef919c4070131939110a35a21073f3b6c
samson0v <vitalikbidochka@gmail.com>
2021-08-26 12:44:37 +0300

    Added multithreading converting data for request connector

--- thingsboard_gateway/connectors/request/json_request_uplink_converter.py
+++ thingsboard_gateway/connectors/request/json_request_uplink_converter.py
@@ -12,6 +12,7 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+from time import time
 from simplejson import dumps, loads
 from thingsboard_gateway.connectors.request.request_converter import RequestConverter, log
 from thingsboard_gateway.tb_utility.tb_utility import TBUtility
@@ -29,13 +30,17 @@ class JsonRequestUplinkConverter(RequestConverter):
         dict_result = {"deviceName": None, "deviceType": None, "attributes": [], "telemetry": []}
         try:
             if self.__config['converter'].get("deviceNameJsonExpression") is not None:
-                dict_result["deviceName"] = TBUtility.get_value(self.__config['converter'].get("deviceNameJsonExpression"), data, expression_instead_none=True)
+                dict_result["deviceName"] = TBUtility.get_value(
+                    self.__config['converter'].get("deviceNameJsonExpression"), data, expression_instead_none=True)
             else:
-                log.error("The expression for looking \"deviceName\" not found in config %s", dumps(self.__config['converter']))
+                log.error("The expression for looking \"deviceName\" not found in config %s",
+                          dumps(self.__config['converter']))
             if self.__config['converter'].get("deviceTypeJsonExpression") is not None:
-                dict_result["deviceType"] = TBUtility.get_value(self.__config['converter'].get("deviceTypeJsonExpression"), data, expression_instead_none=True)
+                dict_result["deviceType"] = TBUtility.get_value(
+                    self.__config['converter'].get("deviceTypeJsonExpression"), data, expression_instead_none=True)
             else:
-                log.error("The expression for looking \"deviceType\" not found in config %s", dumps(self.__config['converter']))
+                log.error("The expression for looking \"deviceType\" not found in config %s",
+                          dumps(self.__config['converter']))
         except Exception as e:
             log.exception(e)
 
@@ -43,13 +48,23 @@ class JsonRequestUplinkConverter(RequestConverter):
             for datatype in self.__datatypes:
                 current_datatype = self.__datatypes[datatype]
                 for datatype_object_config in self.__config["converter"].get(datatype, []):
-                    datatype_object_config_key = TBUtility.get_value(datatype_object_config["key"], data, datatype_object_config["type"], expression_instead_none=True)
-                    datatype_object_config_value = TBUtility.get_value(datatype_object_config["value"], data, datatype_object_config["type"])
+                    datatype_object_config_key = TBUtility.get_value(datatype_object_config["key"], data,
+                                                                     datatype_object_config["type"],
+                                                                     expression_instead_none=True)
+                    datatype_object_config_value = TBUtility.get_value(datatype_object_config["value"], data,
+                                                                       datatype_object_config["type"])
                     if datatype_object_config_key is not None and datatype_object_config_value is not None:
-                        dict_result[current_datatype].append({datatype_object_config_key: datatype_object_config_value})
+                        if data.get("ts") is not None or data.get("timestamp") is not None:
+                            dict_result[self.__datatypes[datatype]].append(
+                                {"ts": data.get('ts', data.get('timestamp', int(time()))),
+                                 'values': {datatype_object_config_key: datatype_object_config_value}})
+                        else:
+                            dict_result[current_datatype].append(
+                                {datatype_object_config_key: datatype_object_config_value})
                     else:
                         error_string = "Cannot find the key in the input data" if datatype_object_config_key is None else "Cannot find the value from the input data"
                         log.error(error_string)
+
         except Exception as e:
             log.exception(e)
 
