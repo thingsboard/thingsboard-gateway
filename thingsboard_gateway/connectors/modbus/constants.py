4d74282f15daa876e822bc99bb6fc00d13bd48f5
zbeacon <ibarkov.ua@gmail.com>
2021-09-14 12:20:24 +0300

    Added import for constants

--- thingsboard_gateway/connectors/modbus/constants.py
+++ thingsboard_gateway/connectors/modbus/constants.py
@@ -12,6 +12,8 @@
 #      See the License for the specific language governing permissions and
 #      limitations under the License.
 
+from thingsboard_gateway.gateway.constants import *
+
 # Connector constants
 
 LAST_PREFIX = "last_"
