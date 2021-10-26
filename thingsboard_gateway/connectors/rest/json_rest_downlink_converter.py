b9101d722d68d310a95ecb231176f03c07bf93a1
samson0v <vitalikbidochka@gmail.com>
2021-10-26 13:49:01 +0300

    Added opportunity to combine values in REST RPC

--- thingsboard_gateway/connectors/rest/json_rest_downlink_converter.py
+++ thingsboard_gateway/connectors/rest/json_rest_downlink_converter.py
@@ -43,20 +43,26 @@ class JsonRESTDownlinkConverter(RESTConverter):
 
                 result = {
                     "url": self.__config["requestUrlExpression"].replace("${restId}", rest_id)
-                                                                .replace("${methodName}", method_name)
-                                                                .replace("${deviceName}", quote(data["device"])),
+                        .replace("${methodName}", method_name)
+                        .replace("${deviceName}", quote(data["device"])),
                     "data": self.__config["valueExpression"].replace("${restId}", rest_id)
-                                                            .replace("${methodName}", method_name)
-                                                            .replace("${deviceName}", quote(data["device"]))
+                        .replace("${methodName}", method_name)
+                        .replace("${deviceName}", quote(data["device"]))
                 }
 
                 result['url'] = TBUtility.replace_params_tags(result['url'], data)
 
-                data_tag = '${' + TBUtility.get_value(config.get('valueExpression'), data['data'], 'params',
-                                                      get_tag=True) + '}'
-                data_value = TBUtility.get_value(config.get('valueExpression'), data['data'], 'params',
-                                                 expression_instead_none=True)
-                result['data'] = result["data"].replace(data_tag, str(data_value))
+                # data_tag = '${' + TBUtility.get_value(config.get('valueExpression'), data['data'], 'params',
+                #                                       get_tag=True) + '}'
+                # data_value = TBUtility.get_value(config.get('valueExpression'), data['data'], 'params',
+                #                                  expression_instead_none=True)
+                data_tags = TBUtility.get_values(config.get('valueExpression'), data['data'], 'params',
+                                                 get_tag=True)
+                data_values = TBUtility.get_values(config.get('valueExpression'), data['data'], 'params',
+                                                   expression_instead_none=True)
+
+                for (tag, value) in zip(data_tags, data_values):
+                    result['data'] = result["data"].replace('${' + tag + '}', str(value))
 
             return result
         except Exception as e:
