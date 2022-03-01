ffaefa331cfc6ec19816b1136f87f3cc82263be8
mattrantz <91554315+mattrantz@users.noreply.github.com>
2022-03-01 15:29:20 -0500

    Update test_mqtt.py

--- zigbee2mqtt/test_mqtt.py
+++ zigbee2mqtt/test_mqtt.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
 import paho.mqtt.client as mqtt
 
-TEMP_ACCESS_TOKEN = "RTkN61pnPIb7Hp4PtdRJ"
-MOTION_ACCESS_TOKEN = "UIjn08YIkHjfE4jzF8N3"
-
-# The callback for when the client receives a CONNACK response from the server
+# Function Setup
+TEMP_ACCESS_TOKEN = "LBkmUN20TdUNE5SZ3E1j"
 def on_connect_local_temp(client, userdata, flags, rc):
     print("Temperature Connected with result code "+str(rc))
+    client.subscribe("zigbee2mqtt/0x00158d0006bcd386")
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("zigbee2mqtt/0x00158d00044f0657")
-
-# The callback for when the client receives a CONNACK response from the server
-def on_connect_local_motion(client, userdata, flags, rc):
-    print("Motion Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("zigbee2mqtt/0x00158d0002a3516b")
-
-# The callback for when a PUBLISH message is received from the server
 def on_message_temp(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     temp_thingsboard_client.publish(f"v1/devices/me/telemetry", payload=msg.payload)
     print("Published message")
 
-# The callback for when a PUBLISH message is received from the server
+
+MOTION_ACCESS_TOKEN = "LjMfJgL9la3yjvSkGGmR"
+def on_connect_local_motion(client, userdata, flags, rc):
+    print("Motion Connected with result code "+str(rc))
+    client.subscribe("zigbee2mqtt/0x00158d0002a3516b")
+
 def on_message_motion(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     motion_thingsboard_client.publish(f"v1/devices/me/telemetry", payload=msg.payload)
     print("Published message")
+    
+ILLUMINANCE_ACCESS_TOKEN = "21ZRM6w1WlwiwsOeZ7Gf"
+def on_connect_local_illuminance(client, userdata, flags, rc):
+    print("Illuminance Connected with result code "+str(rc))
+    client.subscribe("zigbee2mqtt/0x00158d000365879b")
 
+def on_message_illuminance(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    illuminance_thingsboard_client.publish(f"v1/devices/me/telemetry", payload=msg.payload)
+    print("Published message")
+
+# Mosquitto Connections
 local_client_temp = mqtt.Client()
 local_client_temp.on_connect = on_connect_local_temp
 local_client_temp.on_message = on_message_temp
@@ -42,6 +44,12 @@ local_client_motion.on_connect = on_connect_local_motion
 local_client_motion.on_message = on_message_motion
 local_client_motion.connect("localhost", 1883, 60)
 
+local_client_illuminance = mqtt.Client()
+local_client_illuminance.on_connect = on_connect_local_illuminance
+local_client_illuminance.on_message = on_message_illuminance
+local_client_illuminance.connect("localhost", 1883, 60)
+
+# ThingsBoard Connections
 temp_thingsboard_client = mqtt.Client()
 temp_thingsboard_client.username_pw_set(TEMP_ACCESS_TOKEN)
 temp_thingsboard_client.connect("variot.ece.drexel.edu", 1883, 60)
@@ -50,10 +58,19 @@ motion_thingsboard_client = mqtt.Client()
 motion_thingsboard_client.username_pw_set(MOTION_ACCESS_TOKEN)
 motion_thingsboard_client.connect("variot.ece.drexel.edu", 1883, 60)
 
+illuminance_thingsboard_client = mqtt.Client()
+illuminance_thingsboard_client.username_pw_set(ILLUMINANCE_ACCESS_TOKEN)
+illuminance_thingsboard_client.connect("variot.ece.drexel.edu", 1883, 60)
+
+# Loop Starts
 local_client_temp.loop_start()
-local_client_motion.loop_start()
 temp_thingsboard_client.loop_start()
+
+local_client_motion.loop_start()
 motion_thingsboard_client.loop_start()
+
+local_client_illuminance.loop_start()
+illuminance_thingsboard_client.loop_start()
 
 try:
     while True:
@@ -62,6 +79,10 @@ except KeyboardInterrupt:
     pass
 
 local_client_temp.loop_stop()
-local_client_motion.loop_stop()
 temp_thingsboard_client.loop_stop()
+
+local_client_motion.loop_stop()
 motion_thingsboard_client.loop_stop()
+
+local_client_illuminance.loop_stop()
+illuminance_thingsboard_client.loop_stop()
