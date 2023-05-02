cc89caed09c3e487bc033cc6fb7d92a4bf6599ee
samson0v <vitalikbidochka@gmail.com>
2023-05-02 13:54:21 +0300

    Fixed FTP Converter

--- thingsboard_gateway/gateway/duplicate_detector.py
+++ thingsboard_gateway/gateway/duplicate_detector.py
@@ -39,60 +39,61 @@ class DuplicateDetector:
         raise NotImplementedError("Persisting feature for latest attributes/telemetry values is not implemented!")
 
     def filter_data(self, connector_name, new_data):
-        in_data_filter_enabled = new_data.get(SEND_ON_CHANGE_PARAMETER)
-        if not in_data_filter_enabled or not isinstance(in_data_filter_enabled, bool):
-            return new_data
-
-        ttl = new_data.get(SEND_ON_CHANGE_TTL_PARAMETER)
-        device_name = new_data[DEVICE_NAME_PARAMETER]
-        if not in_data_filter_enabled:
-            connector = self._connectors.get(connector_name)
-            if not connector or not connector.is_filtering_enable(device_name):
+        if new_data:
+            in_data_filter_enabled = new_data.get(SEND_ON_CHANGE_PARAMETER)
+            if not in_data_filter_enabled or not isinstance(in_data_filter_enabled, bool):
                 return new_data
-            elif ttl is None:
-                ttl = connector.get_ttl_for_duplicates(device_name)
 
-        now = int(time() * 1000)
-        to_send = {ATTRIBUTES_PARAMETER: [], TELEMETRY_PARAMETER: []}
+            ttl = new_data.get(SEND_ON_CHANGE_TTL_PARAMETER)
+            device_name = new_data[DEVICE_NAME_PARAMETER]
+            if not in_data_filter_enabled:
+                connector = self._connectors.get(connector_name)
+                if not connector or not connector.is_filtering_enable(device_name):
+                    return new_data
+                elif ttl is None:
+                    ttl = connector.get_ttl_for_duplicates(device_name)
 
-        remaining_attributes_count = 0
-        filtered_attributes_count = 0
-        for attribute in new_data[ATTRIBUTES_PARAMETER]:
-            for key, new_value in attribute.items():
-                if self._update_latest_attribute_value(device_name, key, new_value, now, ttl):
-                    to_send[ATTRIBUTES_PARAMETER].append(attribute)
-                    remaining_attributes_count += 1
-                else:
-                    filtered_attributes_count += 1
+            now = int(time() * 1000)
+            to_send = {ATTRIBUTES_PARAMETER: [], TELEMETRY_PARAMETER: []}
 
-        remaining_telemetry_count = 0
-        filtered_telemetry_count = 0
-        for ts_kv_list in new_data[TELEMETRY_PARAMETER]:
-            ts_added = False
-            ts = ts_kv_list.get(TELEMETRY_TIMESTAMP_PARAMETER)
-            ts_values = {}
-            for key, new_value in ts_kv_list.get(TELEMETRY_VALUES_PARAMETER, ts_kv_list).items():
-                if self._update_latest_telemetry_value(device_name, key, new_value, ts if ts else now, ttl):
-                    ts_values[key] = new_value
-                    ts_added = True
-                    remaining_telemetry_count += 1
-                else:
-                    filtered_telemetry_count += 1
-            if ts_added:
-                to_send[TELEMETRY_PARAMETER].append(
-                    {TELEMETRY_TIMESTAMP_PARAMETER: ts, TELEMETRY_VALUES_PARAMETER: ts_values} if ts else ts_values)
+            remaining_attributes_count = 0
+            filtered_attributes_count = 0
+            for attribute in new_data[ATTRIBUTES_PARAMETER]:
+                for key, new_value in attribute.items():
+                    if self._update_latest_attribute_value(device_name, key, new_value, now, ttl):
+                        to_send[ATTRIBUTES_PARAMETER].append(attribute)
+                        remaining_attributes_count += 1
+                    else:
+                        filtered_attributes_count += 1
 
-        if remaining_attributes_count > 0 or remaining_telemetry_count > 0:
-            log.debug("[%s] '%s' changed attributes %d from %d, changed telemetry %d from %d",
-                      connector_name, device_name,
-                      remaining_attributes_count, remaining_attributes_count + filtered_attributes_count,
-                      remaining_telemetry_count, remaining_telemetry_count + filtered_telemetry_count)
-            to_send[DEVICE_NAME_PARAMETER] = device_name
-            to_send[DEVICE_TYPE_PARAMETER] = new_data[DEVICE_TYPE_PARAMETER]
-            return to_send
+            remaining_telemetry_count = 0
+            filtered_telemetry_count = 0
+            for ts_kv_list in new_data[TELEMETRY_PARAMETER]:
+                ts_added = False
+                ts = ts_kv_list.get(TELEMETRY_TIMESTAMP_PARAMETER)
+                ts_values = {}
+                for key, new_value in ts_kv_list.get(TELEMETRY_VALUES_PARAMETER, ts_kv_list).items():
+                    if self._update_latest_telemetry_value(device_name, key, new_value, ts if ts else now, ttl):
+                        ts_values[key] = new_value
+                        ts_added = True
+                        remaining_telemetry_count += 1
+                    else:
+                        filtered_telemetry_count += 1
+                if ts_added:
+                    to_send[TELEMETRY_PARAMETER].append(
+                        {TELEMETRY_TIMESTAMP_PARAMETER: ts, TELEMETRY_VALUES_PARAMETER: ts_values} if ts else ts_values)
 
-        log.info("[%s] '%s' device data has not been changed", connector_name, device_name)
-        return None
+            if remaining_attributes_count > 0 or remaining_telemetry_count > 0:
+                log.debug("[%s] '%s' changed attributes %d from %d, changed telemetry %d from %d",
+                          connector_name, device_name,
+                          remaining_attributes_count, remaining_attributes_count + filtered_attributes_count,
+                          remaining_telemetry_count, remaining_telemetry_count + filtered_telemetry_count)
+                to_send[DEVICE_NAME_PARAMETER] = device_name
+                to_send[DEVICE_TYPE_PARAMETER] = new_data[DEVICE_TYPE_PARAMETER]
+                return to_send
+
+            log.info("[%s] '%s' device data has not been changed", connector_name, device_name)
+            return None
 
     @staticmethod
     def _create_device_latest_data():
