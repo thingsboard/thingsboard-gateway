65e756786adb209c7c818589a0c3ae67e6181347
imbeacon <ibarkov.ua@gmail.com>
2023-05-26 11:57:02 +0300

    Revert "Fixed version resolving"
    This reverts commit f2bbe4e353f98a14b06f312a9fd164966c2808d3.

--- setup.py
+++ setup.py
@@ -17,12 +17,11 @@
 from setuptools import setup
 from os import path
 
-from thingsboard_gateway.gateway.constants import VERSION
-
-
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+VERSION = "3.2"
 
 setup(
     version=VERSION,
