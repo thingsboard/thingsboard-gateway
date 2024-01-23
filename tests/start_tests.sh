a4b34960c59b3079fb70d31b3885e0489ed84537
imbeacon <ibarkov.ua@gmail.com>
2024-01-23 14:36:31 +0200

    Updated start script

--- tests/start_tests.sh
+++ tests/start_tests.sh
@@ -5,10 +5,10 @@ pip install -r requirements.txt --no-cache-dir
 pip install pyjwt==2.6.0 --no-cache-dir
 pip install tb-rest-client --no-cache-dir
 cp docker/Dockerfile .
-#docker build -t tb-gateway --load .
+docker build -t tb-gateway --load .
 docker compose --file tests/docker-compose.yml up -d
 sleep 60
 python3 -m unittest discover -s . -p 'test_*.py' -v
 docker compose down
-#deactivate
-#rm -rf venv
+deactivate
+rm -rf venv
