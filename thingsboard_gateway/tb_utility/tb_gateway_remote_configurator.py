da5bed99fbd560d5609109c7c0aa52ff13b28ed4
samson0v <vitalikbidochka@gmail.com>
2024-04-22 16:34:08 +0300

    Deleted sending default connectors config feature

--- thingsboard_gateway/tb_utility/tb_gateway_remote_configurator.py
+++ thingsboard_gateway/tb_utility/tb_gateway_remote_configurator.py
@@ -17,12 +17,10 @@ from logging import getLogger
 from logging.config import dictConfig
 from time import sleep, time
 
-from packaging import version
 from regex import fullmatch
 from simplejson import dumps, load
 
 from thingsboard_gateway.gateway.tb_client import TBClient
-from thingsboard_gateway.tb_utility.tb_handler import TBLoggerHandler
 from thingsboard_gateway.tb_utility.tb_utility import TBUtility
 
 LOG = getLogger("service")
@@ -54,9 +52,6 @@ class RemoteConfigurator:
             'logs_configuration': 'logs.json'
         }
         self._max_backup_files_number = 10
-
-        self._remote_gateway_version = None
-        self._fetch_remote_gateway_version()
 
         # creating backups (general and connectors)
         self.create_configuration_file_backup(self._get_general_config_in_local_format(), "tb_gateway.json")
@@ -95,59 +90,6 @@ class RemoteConfigurator:
             connector.pop('config_updated', None)
             connector.pop('config_file_path', None)
         return connectors
-
-    def _fetch_remote_gateway_version(self):
-        def callback(key, err):
-            if err is not None:
-                LOG.exception(err)
-            if key is None:
-                self._remote_gateway_version = '0.0'
-                return
-
-            try:
-                self._remote_gateway_version = key['client']['Version']
-            except KeyError:
-                self._remote_gateway_version = '0.0'
-                LOG.warning('Cannot fetch version number from server! Version number set to 0.0')
-
-        self._gateway.tb_client.client.request_attributes(client_keys=['Version'], callback=callback)
-
-    def _send_default_connectors_config(self):
-        """
-        If remote gateway version wasn't fetch (default set to '0.0'), remote configurator send all default
-        connectors configs.
-        """
-        from thingsboard_gateway.gateway.tb_gateway_service import DEFAULT_CONNECTORS
-
-        # remote gateway version fetching in __init__ method (_fetch_remote_gateway_version)
-        LOG.debug('Waiting for remote gateway version...')
-
-        try_count = 1
-        while not self._remote_gateway_version and try_count <= 3:
-            if self._gateway.stopped:
-                return
-            try_count += 1
-            sleep(1)
-
-        if self._remote_gateway_version is None:
-            self._remote_gateway_version = "0.0"
-
-        need_update_configs = (self._remote_gateway_version == "0.0"
-                               or version.parse(self._gateway.version.get('current_version', '0.0')) > version.parse(str(self._remote_gateway_version)))
-
-        if need_update_configs:
-            default_connectors_configs_folder_path = self._gateway.get_config_path() + 'default-configs/'
-
-            for (connector_type, _) in DEFAULT_CONNECTORS.items():
-                connector_filename = connector_type + '.json'
-                try:
-                    with open(default_connectors_configs_folder_path + connector_filename, 'r') as file:
-                        config = load(file)
-                        self._gateway.tb_client.client.send_attributes(
-                            {connector_type.upper() + '_DEFAULT_CONFIG': config})
-                        LOG.debug('Default config for %s connector sent.', connector_type)
-                except FileNotFoundError:
-                    LOG.error('Default config file for %s connector not found! Passing...', connector_type)
 
     def _get_active_connectors(self):
         return [connector['name'] for connector in self.connectors_configuration]
@@ -215,7 +157,6 @@ class RemoteConfigurator:
             'Version': self._gateway.version.get('current_version', '0.0')
         }
         self._gateway.tb_client.client.send_attributes(init_config_message)
-        self._send_default_connectors_config()
 
         # sending remote created connectors
         for connector in self.connectors_configuration:
