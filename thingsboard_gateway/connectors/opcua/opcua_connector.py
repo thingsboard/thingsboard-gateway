8501c38493816816abc487d8b3da781bab091569
Gonzalo Moran <gonzalo.moran@horseit.tech>
2024-08-12 21:24:08 +0200

    * Asyncio disconnect handled

--- thingsboard_gateway/connectors/opcua/opcua_connector.py
+++ thingsboard_gateway/connectors/opcua/opcua_connector.py
@@ -113,34 +113,36 @@ class OpcUaConnector(Connector, Thread):
         return self._connector_type
 
     def close(self):
-        try:
-            self.__stopped = True
-            self.__connected = False
-            self.__log.info("Stopping OPC-UA Connector")
+        self.__stopped = True          
+        self.__connected = False
+        self.__log.info("Stopping OPC-UA Connector")
 
-            asyncio.run_coroutine_threadsafe(self.__cancel_all_tasks(), self.__loop)
+        asyncio.run_coroutine_threadsafe(self.__cancel_all_tasks(), self.__loop)
 
-            start_time = monotonic()
+        asyncio.run(self.__disconnect())
 
-            while self.is_alive():
-                if monotonic() - start_time > 5:
-                    self.__log.error("Failed to stop connector %s", self.get_name())
-                    break
-                sleep(.1)
+        start_time = monotonic()
 
-            self.__log.info('%s has been stopped.', self.get_name())
-            self.__log.stop()
+        while self.is_alive():
+            if monotonic() - start_time > 5:
+                self.__log.error("Failed to stop connector %s", self.get_name())
+                break
+            sleep(.1)
 
-        finally:
-            try:
-                self.__client.disconnect()
-                self.__log.info(f'Disconnected from OPC-UA Server')
-            except:
-                self.__log.warning(f'Could not disconnect from OPC-UA Server')
+        self.__log.info('%s has been stopped.', self.get_name())
+        self.__log.stop()
+            
 
     async def __cancel_all_tasks(self):
         for task in asyncio.all_tasks(self.__loop):
             task.cancel()
+
+    async def __disconnect(self):
+        try:
+            await self.__client.disconnect()
+            self.__log.info('%s has been disconnected from OPC-UA Server.', self.get_name())
+        except:
+             self.__log.warning(f'{self.get_name()} could not be disconnected from OPC-UA Server.')
 
     async def __reset_node(self, node):
         node['valid'] = False
