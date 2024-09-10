66c44646cfe11ca0ee2c937362ae0b776a0295af
Sergey Matvienko <smatvienko@thingsboard.io>
2024-09-10 17:17:38 +0200

    OPCUA converter timestamp = int(time() * 1000)

--- thingsboard_gateway/connectors/opcua/opcua_uplink_converter.py
+++ thingsboard_gateway/connectors/opcua/opcua_uplink_converter.py
@@ -67,7 +67,7 @@ class OpcUaUplinkConverter(OpcUaConverter):
             telemetry_datapoints_count = 0
             attributes_datapoints_count = 0
 
-            basic_timestamp = int(time() * 1000)
+            timestamp = int(time() * 1000)
 
             for val, config in zip(values, configs):
                 if not val:
@@ -90,16 +90,6 @@ class OpcUaUplinkConverter(OpcUaConverter):
                     data = msg
 
                 section = DATA_TYPES[config['section']]
-
-                if val.SourceTimestamp is not None:
-                    if abs(basic_timestamp - val.SourceTimestamp.timestamp()) > 3600:
-                        self._log.warning("Timestamps are not in sync for incoming value: %r. "
-                                          "Value timestamp: %s, current timestamp: %s",
-                                          val, val.SourceTimestamp.timestamp(), basic_timestamp)
-                    else:
-                        basic_timestamp = val.SourceTimestamp.timestamp() * 1000
-
-                timestamp = basic_timestamp
 
                 if val.SourceTimestamp and section == TELEMETRY_PARAMETER:
                     telemetry_datapoints_count += 1
