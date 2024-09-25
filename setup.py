1e5f5d81c311c21fdac8698a839acc91d4ca5aa7
imbeacon <ibarkov.ua@gmail.com>
2024-09-25 09:42:26 +0300

    Updated SDK version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'grpcio',
         'protobuf',
         'cachetools',
-        'tb-mqtt-client>=1.10.1',
+        'tb-mqtt-client>=1.10.2',
         'packaging==23.1',
         'service-identity',
         'psutil'
