7d15cb482779976e50f178246577e7f5fefaa457
imbeacon <ibarkov.ua@gmail.com>
2024-11-01 13:12:49 +0200

    Adjusted tests to be compatible with ConvertedData entity

--- tests/unit/converters/odbc/test_odbc_uplink_converter.py
+++ tests/unit/converters/odbc/test_odbc_uplink_converter.py
@@ -33,40 +33,96 @@ class OdbcUplinkConverterTests(BaseUnitTest):
                         "stringValue": "".join(choice(ascii_lowercase) for _ in range(8))}
 
     def test_glob_matching(self):
-        converted_data = self.converter.convert("*", self.db_data)
-        self.assertDictEqual(converted_data, self.db_data)
+        data = {"deviceName": "testDevice",
+                "deviceType": "testType",
+                "attributes": {"key": "value"},
+                "telemetry": [{"value": 42, "ts": 1234567890}]}
+        converted_data = self.converter.convert("*", data)
+
+        self.assertEqual(converted_data.device_name, data["deviceName"])
+        self.assertEqual(converted_data.device_type, data["deviceType"])
+        self.assertDictEqual(converted_data.to_dict().get('attributes'), data["attributes"])
+        self.assertDictEqual(converted_data.telemetry[0].values, data["telemetry"][0])
 
     def test_data_subset(self):
-        config = ["floatValue", "boolValue"]
+        config = {
+            "attributes": "*",
+            "timeseries": [
+              {
+                "name": "value1",
+                "value": "floatValue"
+              },
+              {
+                "name": "value2",
+                "value": "boolValue"
+              }
+            ]
+          }
         converted_data = self.converter.convert(config, self.db_data)
-        expected_data = {}
-        for key in config:
-            expected_data[key] = self.db_data[key]
-        self.assertDictEqual(converted_data, expected_data)
+        expected_telemetry_keys = ["value1", "value2"]
+        expected_attributes_keys = ["boolValue", "intValue", "floatValue", "stringValue"]
+        for attribute in converted_data.attributes:
+            self.assertIn(attribute.key, expected_attributes_keys)
+        for telemetry_entry in converted_data.telemetry:
+            for telemetry in telemetry_entry.values.keys():
+                self.assertIn(telemetry.key, expected_telemetry_keys)
 
     def test_alias(self):
-        config = [{"column": "stringValue", "name": "valueOfString"}]
+        config = {
+            "attributes": "*",
+            "timeseries": [
+                {
+                    "name": "valueOfString",
+                    "column": "stringValue"
+                }
+            ]
+        }
         converted_data = self.converter.convert(config, self.db_data)
-        self.assertDictEqual(converted_data, {config[0]["name"]: self.db_data[config[0]["column"]]})
+        self.assertEqual(list(converted_data.telemetry[0].values.keys())[0].key, config["timeseries"][0]["name"])
 
     def test_name_expression(self):
+        config = {
+            "attributes": [{
+                "nameExpression": "key",
+                "value": "intValue"
+            }],
+            "timeseries": []
+        }
         attr_name = "someAttribute"
-        config = [{"nameExpression": "key", "value": "intValue"}]
         self.db_data["key"] = attr_name
         converted_data = self.converter.convert(config, self.db_data)
-        self.assertDictEqual(converted_data, {attr_name: self.db_data[config[0]["value"]]})
+        self.assertEqual(list(converted_data.attributes.values.keys())[0].key, attr_name)
 
     def test_value_config(self):
-        config = [{"name": "someValue", "value": "stringValue + str(intValue)"}]
+        config = {
+            "attributes": [],
+            "timeseries": [
+                {
+                    "name": "someValue",
+                    "value": "stringValue + str(intValue)"
+                }
+            ]
+        }
         converted_data = self.converter.convert(config, self.db_data)
-        self.assertDictEqual(converted_data, {config[0]["name"]: self.db_data["stringValue"] + str(self.db_data["intValue"])})
+        self.assertEqual(list(converted_data.telemetry[0].values.values())[0], self.db_data["stringValue"] + str(self.db_data["intValue"]))
 
     def test_one_valid_one_invalid_configs(self):
-        config = ["unkownColumnValue", "stringValue"]
+        config = {
+            "attributes": [{
+                "name": "key",
+                "column": "unkownColumnValue"
+            },
+            {
+                "name": "str",
+                "value": "stringValue"
+            }],
+            "timeseries": []
+        }
         with self.assertLogs(level="ERROR") as log:
             converted_data = self.converter.convert(config, self.db_data)
             self.assertTrue(log.output, "Failed to convert SQL data to TB format: 'unkownColumnValue'")
-        self.assertDictEqual(converted_data, {config[1]: self.db_data[config[1]]})
+        self.assertEqual(len(converted_data.attributes.values), 1)
+        self.assertEqual(list(converted_data.attributes.values.keys())[0].key, config["attributes"][1]["name"])
 
 
 if __name__ == '__main__':
