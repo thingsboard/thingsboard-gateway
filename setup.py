9ed5affc46a8ff7b9772376aa182a9e1c61753af
imbeacon <ibarkov.ua@gmail.com>
2025-04-08 13:11:11 +0300

    Updated requirements due to changes in SDK

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
         'protobuf',
         'cachetools',
         'tb-paho-mqtt-client>=2.1.2',
-        'tb-mqtt-client>=1.13.2',
+        'tb-mqtt-client>=1.13.3',
         'packaging==23.1',
         'service-identity',
         'psutil'
