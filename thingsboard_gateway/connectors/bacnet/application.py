4fd4d3da00f58bc4e5b6fc9e006825b0e81ae55a
samson0v <vitalikbidochka@gmail.com>
2025-04-28 14:46:10 +0300

    Added BACnet device objects autodiscovery

--- thingsboard_gateway/connectors/bacnet/application.py
+++ thingsboard_gateway/connectors/bacnet/application.py
@@ -12,9 +12,14 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+from typing import List
 from bacpypes3.ipv4.app import NormalApplication as App
 from bacpypes3.local.device import DeviceObject
 from bacpypes3.pdu import Address
+from bacpypes3.primitivedata import ObjectIdentifier
+from bacpypes3.apdu import AbortReason, AbortPDU, ErrorRejectAbortNack
+from bacpypes3.basetypes import PropertyIdentifier
+from bacpypes3.vendor import VendorInfo
 
 from thingsboard_gateway.connectors.bacnet.entities.device_object_config import DeviceObjectConfig
 
@@ -31,10 +36,90 @@ class Application(App):
     async def indication(self, apdu) -> None:
         self.__log.debug(f"(indication) Received APDU: {apdu}")
         await super().indication(apdu)
-        self.__indication_callback(apdu)
+        await self.__indication_callback(apdu)
 
     async def do_who_is(self, device_address):
         devices = await self.who_is(address=Address(device_address),
                                     timeout=self.__device_object_config.device_discovery_timeout)
         if len(devices):
             return devices[0]
+
+    async def get_object_identifiers(
+        self, device_address: Address, device_identifier: ObjectIdentifier
+    ) -> List[ObjectIdentifier]:
+        try:
+            object_list = await self.read_property(
+                device_address, device_identifier, "objectList"
+            )
+            return object_list
+        except AbortPDU as err:
+            if err.apduAbortRejectReason != AbortReason.segmentationNotSupported:
+                self.__log.warning(f"{device_identifier} objectList abort: {err}\n")
+                return []
+        except ErrorRejectAbortNack as err:
+            self.__log.warning(f"{device_identifier} objectList error/reject: {err}\n")
+            return []
+
+        object_list = []
+        try:
+            object_list_length = await self.read_property(
+                device_address,
+                device_identifier,
+                "objectList",
+                array_index=0,
+            )
+
+            for i in range(object_list_length):
+                object_identifier = await self.read_property(
+                    device_address,
+                    device_identifier,
+                    "objectList",
+                    array_index=i + 1,
+                )
+                object_list.append(object_identifier)
+        except ErrorRejectAbortNack as err:
+            self.__log.info(f"{device_identifier} object-list length error/reject: {err}\n")
+
+        return object_list
+
+    async def get_device_objects(self,
+                                 device_address: Address,
+                                 device_identifier: ObjectIdentifier,
+                                 vendor_info: VendorInfo):
+        result = []
+
+        object_list = await self.get_object_identifiers(device_address, device_identifier)
+
+        for object_id in object_list:
+            config = {}
+
+            object_class = vendor_info.get_object_class(object_id[0])
+
+            if object_class is None:
+                self.__log.warning(f"unknown object type: {object_id}\n")
+                continue
+
+            try:
+                property_identifier = PropertyIdentifier('object-name')
+
+                property_class = object_class.get_property_type(property_identifier)
+                if property_class is None:
+                    self.__log.warning(f"{object_id} unknown property: {property_identifier}\n")
+                    continue
+
+                property_value = await self.read_property(
+                    device_address, object_id, property_identifier
+                )
+
+                config = {
+                    'objectType': object_id[0].__str__(),
+                    'objectId': object_id[-1],
+                    'propertyId': 'presentValue',
+                    'key': property_value,
+                }
+            except ErrorRejectAbortNack as err:
+                self.__log.warning(f"{object_id} object-name error: {err}\n")
+            else:
+                result.append(config)
+
+        return result
--- thingsboard_gateway/connectors/bacnet/bacnet_connector.py
+++ thingsboard_gateway/connectors/bacnet/bacnet_connector.py
@@ -35,6 +35,7 @@ except ImportError:
     from bacpypes3.apdu import ErrorRejectAbortNack
 
 from bacpypes3.pdu import Address
+from bacpypes3.vendor import get_vendor_info
 
 from thingsboard_gateway.connectors.bacnet.device import Device
 from thingsboard_gateway.connectors.bacnet.entities.device_object_config import DeviceObjectConfig
@@ -109,7 +110,7 @@ class AsyncBACnetConnector(Thread, Connector):
         await self.__discover_devices()
         await asyncio.gather(self.__main_loop(), self.__convert_data(), self.__save_data())
 
-    def indication_callback(self, apdu):
+    async def indication_callback(self, apdu):
         """
         First check if device is already added
         If added, set active to True
@@ -123,6 +124,8 @@ class AsyncBACnetConnector(Thread, Connector):
             if added_device is None:
                 device_config = Device.find_self_in_config(self.__config['devices'], apdu)
                 if device_config:
+                    await self.__check_and_update_device_config(apdu, device_config)
+
                     device = Device(self.connector_type, device_config, apdu, self.callback, self.__converter_log)
                     self.__devices.append(device)
                     self.__gateway.add_device(device.device_info.device_name,
@@ -141,6 +144,16 @@ class AsyncBACnetConnector(Thread, Connector):
         for device in self.__devices:
             if device.details.address == address or address in device.alternative_responses_addresses:
                 return device
+
+    async def __check_and_update_device_config(self, apdu, device_config):
+        discover_for = Device.is_discovery_config(device_config)
+        if len(discover_for):
+            vendor_info = get_vendor_info(apdu.vendorID)
+            config = await self.__application.get_device_objects(apdu.pduSource,
+                                                                 apdu.iAmDeviceIdentifier,
+                                                                 vendor_info)
+            for section in discover_for:
+                device_config[section] = config
 
     def __find_device_by_name(self, name):
         device_filter = list(filter(lambda x: x.device_info.device_name == name, self.__devices))
