88132446f8b18aed3ca0979bc924b7fa8e28ee66
Andry925 <atymoshenko@thingsboard.io>
2025-08-01 16:59:50 +0300

    remove not stable assertion from sqlite storage integration test

--- tests/unit/service/test_storage.py
+++ tests/unit/service/test_storage.py
@@ -199,18 +199,14 @@ class TestSQLiteEventStorageRotation(TestCase):
 
         self.sqlite_storage.stop()
 
-    def test_rotation_creates_new_db_and_reads_all_data(self):
+    def test_rotation_creates_new_db(self):
         DATA_RANGE = 150
-        fat_msg = "X" * 32768
         self._fill_storage(self.sqlite_storage, DATA_RANGE, delay=0.1)
         sleep(2.0)
 
         dbs = self._db_files()
         self.assertEqual(len(dbs), 2)
         self.assertLessEqual(len(dbs), self.config["max_db_amount"])
-        all_messages = self._drain_storage(self.sqlite_storage)
-        self.assertEqual(len(all_messages), DATA_RANGE)
-        self.assertListEqual(all_messages, [f"{i}:{fat_msg}" for i in range(DATA_RANGE)])
 
         self.sqlite_storage.stop()
 
