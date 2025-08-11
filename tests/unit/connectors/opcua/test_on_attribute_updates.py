c0d20df17a5efea581c05a5e892196cab3328484
Andry925 <atymoshenko@thingsboard.io>
2025-08-11 12:42:39 +0300

    add licences to the opcua files

--- tests/unit/connectors/opcua/test_on_attribute_updates.py
+++ tests/unit/connectors/opcua/test_on_attribute_updates.py
@@ -12,7 +12,6 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-
 from asyncio import Future
 from unittest.mock import patch, call
 from thingsboard_gateway.gateway.constants import ON_ATTRIBUTE_UPDATE_DEFAULT_TIMEOUT
