f8477c59631332f0d0543647eb62cb6d2c830f9c
Andry925 <atymoshenko@thingsboard.io>
2025-11-24 18:24:34 +0200

    implement tests for opcua uplink messages

--- tests/blackbox/connectors/opcua/test_opcua_uplink_messages.py
+++ tests/blackbox/connectors/opcua/test_opcua_uplink_messages.py
@@ -1,169 +1,76 @@
 import unittest
 from time import sleep, time
-from unittest import skip
 
 from tests.blackbox.connectors.opcua.test_base_opcua import BaseOpcuaTest
 from tests.test_utils.gateway_device_util import GatewayDeviceUtil
 
 
-@skip('Flaky test')
-class OpcuaAsyncioUplinkMessagesTest(BaseOpcuaTest):
-    def test_gateway_connection(self):
-        self.assertEqual(self.client.get_attributes_by_scope(self.gateway.id, 'SERVER_SCOPE', 'active')[0]['value'],
-                         True)
+class OpcUaUplinkMessagesTest(BaseOpcuaTest):
 
-    @skip('Unimplemented')
-    def test_reading_from_views(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/reading_from_views_config.json')
-        GatewayDeviceUtil.update_connector_config(self.CONNECTOR_NAME, config)
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/reading_from_views_values.json')
-
-        for (_type, value) in expected_values.items():
-            self.assertEqual(value, actual_values[_type][0]['value'],
-                             f'Value is not equal for the next telemetry key: {_type}')
-
-    @skip('Unimplemented')
-    def test_reading_from_variable_in_the_variable(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/reading_from_variable_in_the_variable_config.json')
-        GatewayDeviceUtil.update_connector_config(self.CONNECTOR_NAME, config)
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/reading_from_variable_in_the_variable_values.json')
-
-        for (_type, value) in expected_values.items():
-            self.assertEqual(value, actual_values[_type][0]['value'],
-                             f'Value is not equal for the next telemetry key: {_type}')
-
-    # def test_gateway_restarted(self):
-    #     sleep(8)
-    #     GatewayDeviceUtil.restart_gateway()
-    #     restarted_time = time() * 1000
-    #     config = self.load_configuration(
-    #         self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
-    #     GatewayDeviceUtil.update_connector_config(
-    #         self.CONNECTOR_NAME,
-    #         self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
-    #     sleep(self.GENERAL_TIMEOUT)
-    #     telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for
-    #                       key in node['timeseries']]
-    #     actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-    #     for actual_value in actual_values.values():
-    #         self.assertGreater(actual_value[0]['ts'], restarted_time)
-
-    def test_sending_attrs_and_telemetries_after_connection_lost(self):
-        GatewayDeviceUtil.update_credentials({"credentialsType": "ACCESS_TOKEN",
-                                              "credentialsId": "SOME_ACCESS_TOKEN"})
+    def test_opcua_absolute_path(self):
+        (config, _) = self.change_connector_configuration(
+            self.CONFIG_PATH + 'configs/uplink_configs/opcua_uplink_converter_absolute_path.json')
+        telemetry_keys = [
+            key['key']
+            for conf_list in config[self.CONNECTOR_NAME]['configurationJson']['mapping']
+            for key in conf_list['timeseries']
+        ]
         sleep(self.GENERAL_TIMEOUT)
+        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
+        expected_values = self.load_configuration(
+            self.CONFIG_PATH + 'test_values/uplink/different_paths_finding_methods_values.json'
+        )
+        for (_type, value) in expected_values.items():
+            self.assertEqual(value, actual_values[_type][0]['value'],
+                             f'Value is not equal for the next telemetry key: {_type}')
 
-        connection_lost_start_time = time() * 1000
-
-        GatewayDeviceUtil.update_credentials({"credentialsType": "ACCESS_TOKEN",
-                                              "credentialsId": "YOUR_ACCESS_TOKEN"})
+    def test_opcua_relative_path(self):
+        (config, _) = self.change_connector_configuration(
+            self.CONFIG_PATH + 'configs/uplink_configs/opcua_uplink_converter_relative_path.json')
+        telemetry_keys = [
+            key['key']
+            for conf_list in config[self.CONNECTOR_NAME]['configurationJson']['mapping']
+            for key in conf_list['timeseries']
+        ]
         sleep(self.GENERAL_TIMEOUT)
+        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
+        expected_values = self.load_configuration(
+            self.CONFIG_PATH + 'test_values/uplink/different_paths_finding_methods_values.json'
+        )
+        for (_type, value) in expected_values.items():
+            self.assertEqual(value, actual_values[_type][0]['value'],
+                             f'Value is not equal for the next telemetry key: {_type}')
 
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
+    def test_opcua_with_different_identifier_types(self):
+        (config, _) = self.change_connector_configuration(
+            self.CONFIG_PATH + 'configs/uplink_configs/opcua_uplink_converter_identifier.json')
+        telemetry_keys = [
+            key['key']
+            for conf_list in config[self.CONNECTOR_NAME]['configurationJson']['mapping']
+            for key in conf_list['timeseries']
+        ]
         sleep(self.GENERAL_TIMEOUT)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        for actual_value in actual_values.values():
-            self.assertGreater(actual_value[0]['ts'], connection_lost_start_time)
-
-    def test_different_node_finding_methods_path(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_path.json')
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config[self.CONNECTOR_NAME]['configurationJson']['server']['mapping']
-                          for key in node['timeseries']]
         actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
         expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/different_node_finding_methods_values.json')
-
+            self.CONFIG_PATH + 'test_values/uplink/different_identifier_finding_methods_values.json'
+        )
         for (_type, value) in expected_values.items():
             self.assertEqual(value, actual_values[_type][0]['value'],
                              f'Value is not equal for the next telemetry key: {_type}')
 
-    def test_different_node_finding_methods_i(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_i.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_i.json')
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
+    def test_opcua_with_different_types(self):
+        (config, _) = self.change_connector_configuration(
+            self.CONFIG_PATH + 'configs/uplink_configs/opcua_uplink_converter_different_types.json')
+        telemetry_keys = [
+            key['key']
+            for conf_list in config[self.CONNECTOR_NAME]['configurationJson']['mapping']
+            for key in conf_list['timeseries']
+        ]
+        sleep(self.GENERAL_TIMEOUT)
         actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
         expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/different_node_finding_methods_values.json')
-
-        for (_type, value) in expected_values.items():
-            self.assertEqual(value, actual_values[_type][0]['value'],
-                             f'Value is not equal for the next telemetry key: {_type}')
-
-    def test_different_node_finding_methods_s(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_s.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_s.json')
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/different_node_finding_methods_values.json')
-
-        for (_type, value) in expected_values.items():
-            self.assertEqual(value, actual_values[_type][0]['value'],
-                             f'Value is not equal for the next telemetry key: {_type}')
-
-    def test_different_node_finding_methods_g(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_g.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_g.json')
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/different_node_finding_methods_values.json')
-
-        for (_type, value) in expected_values.items():
-            self.assertEqual(value, actual_values[_type][0]['value'],
-                             f'Value is not equal for the next telemetry key: {_type}')
-
-    def test_different_node_finding_methods_b(self):
-        config = self.load_configuration(
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_b.json')
-        GatewayDeviceUtil.update_connector_config(
-            self.CONNECTOR_NAME,
-            self.CONFIG_PATH + 'configs/uplink_configs/different_node_finding_methods_config_b.json')
-        sleep(5)
-        telemetry_keys = [key['key'] for node in config['Opcua']['configurationJson']['server']['mapping'] for key in
-                          node['timeseries']]
-        actual_values = self.client.get_latest_timeseries(self.device.id, ','.join(telemetry_keys))
-        expected_values = self.load_configuration(
-            self.CONFIG_PATH + 'test_values/uplink/different_node_finding_methods_values.json')
-
+            self.CONFIG_PATH + 'test_values/uplink/different_types_finding_methods_values.json'
+        )
         for (_type, value) in expected_values.items():
             self.assertEqual(value, actual_values[_type][0]['value'],
                              f'Value is not equal for the next telemetry key: {_type}')
