027bf1260b2668969953b7be57427ba8661aa083
Andry925 <atymoshenko@thingsboard.io>
2026-02-20 14:32:08 +0200

    add unit-tests for missing placeholder scenario

--- tests/unit/converters/request/test_request_json_uplink_converter.py
+++ tests/unit/converters/request/test_request_json_uplink_converter.py
@@ -47,3 +47,72 @@ class RequestJsonUplinkConverterTest(BaseUnitTest):
         converter = JsonRequestUplinkConverter(test_request_config, self.log)
         result = converter.convert(test_request_convert_config, test_request_body_to_convert)
         self.assertEqual(expected_result.telemetry[0].values, result.telemetry[0].values)
+
+    def test_convert_missing_placeholder_is_skipped(self):
+        test_request_config = {
+            "url": "/last",
+            "httpMethod": "GET",
+            "httpHeaders": {"ACCEPT": "application/json"},
+            "allowRedirects": True,
+            "timeout": 0.5,
+            "scanPeriod": 5,
+            "converter": {
+                "deviceNameJsonExpression": "SD8500",
+                "deviceTypeJsonExpression": "default",
+                "type": "json",
+                "attributes": [],
+                "telemetry": [
+                    {"key": "temperature", "type": "float", "value": "${temperature}"},
+                    {"key": "humidity", "type": "float", "value": "${humidity}"},  # missing in payload
+                ],
+            },
+        }
+
+        test_request_body_to_convert = {"temperature": 21.5}
+        test_request_convert_config = "127.0.0.1:5000/last"
+
+        expected_result = ConvertedData(device_name="SD8500", device_type="default")
+        telemetry_entry = TelemetryEntry({DatapointKey("temperature"): 21.5})
+        expected_result.add_to_telemetry(telemetry_entry)
+
+        converter = JsonRequestUplinkConverter(test_request_config, self.log)
+        result = converter.convert(test_request_convert_config, test_request_body_to_convert)
+
+        self.assertEqual(len(result.telemetry), 1)
+        self.assertEqual(expected_result.telemetry[0].values, result.telemetry[0].values)
+        self.assertNotIn(DatapointKey("humidity"), result.telemetry[0].values)
+
+    def test_convert_nested_jsonpath_missing_placeholder_is_skipped(self):
+        test_request_config = {
+            "url": "/last",
+            "httpMethod": "GET",
+            "httpHeaders": {"ACCEPT": "application/json"},
+            "allowRedirects": True,
+            "timeout": 0.5,
+            "scanPeriod": 5,
+            "converter": {
+                "deviceNameJsonExpression": "${$.meta.sensor}",
+                "deviceTypeJsonExpression": "default",
+                "type": "json",
+                "attributes": [],
+                "telemetry": [
+                    {"key": "temperature", "type": "float", "value": "${$.data.temperature}"},
+                    {"key": "humidity", "type": "float", "value": "${$.data.humidity}"},
+                ],
+            },
+        }
+
+        test_request_body_to_convert = {
+            "meta": {"sensor": "aranet:358151000412:100886"},
+            "data": {"temperature": 21.5},
+        }
+        test_request_convert_config = "127.0.0.1:5000/last"
+
+        expected_result = ConvertedData(device_name="aranet:358151000412:100886", device_type="default")
+        telemetry_entry = TelemetryEntry({DatapointKey("temperature"): 21.5})
+        expected_result.add_to_telemetry(telemetry_entry)
+
+        converter = JsonRequestUplinkConverter(test_request_config, self.log)
+        result = converter.convert(test_request_convert_config, test_request_body_to_convert)
+        self.assertEqual(len(result.telemetry), 1)
+        self.assertEqual(expected_result.telemetry[0].values, result.telemetry[0].values)
