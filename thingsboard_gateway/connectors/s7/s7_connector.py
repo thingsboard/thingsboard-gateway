7bd8ed44cd16ad9fe0a0796998d03944be21ca59
Prus.Oleksandr <cheepaxxa2003@gmail.com>
2026-09-15 17:31:30 +0300

    Implemented S7 shared attribute sync

--- thingsboard_gateway/connectors/s7/s7_connector.py
+++ thingsboard_gateway/connectors/s7/s7_connector.py
@@ -211,6 +211,11 @@ class S7Connector(Thread, Connector):
                     continue
 
                 results = await device.read_configured_data()
+
+                if not device.is_connected():
+                    self.__delete_device_from_platform(device)
+                    continue
+
                 if len(results) <= 0:
                     self.__log.trace(
                         'No data read from device %s.', device.config.device_name)
@@ -394,6 +399,18 @@ class S7Connector(Thread, Connector):
             if device.config.device_name == device_name:
                 return device
         return None
+
+    def __delete_device_from_platform(self, device):
+        if device.config.device_name in self.__gateway.get_devices(connector_id=self.get_id()):
+            self.__log.warning('Device %s is disconnected, removing it from the platform.',
+                              device.config.device_name)
+            self.__gateway.del_device(device.config.device_name)
+
+    def get_device_shared_attributes_keys(self, device_name):
+        device = self._get_device_by_name(device_name)
+        if device is not None:
+            return device.config.shared_attributes_keys
+        return []
 
     def _process_rpc(self, rpc_method_name, rpc_config, device, req_id=None, value=None, with_reply=True):
         result = None
